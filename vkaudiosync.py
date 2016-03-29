0eb80992a3635c46c6a4bef8c12af06e216a02a9
soshial <soshial.reg@gmail.com>
2016-03-29 23:28:50 +0300

    the script now shows playlist changes since last sync: all deleted and new tracks; saving playlists needed timeout; interrupted downloads are handled correctly now; some title regexp were improved;

--- vkaudiosync.py
+++ vkaudiosync.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
+# encoding: utf-8
 import os
-import sys
 import urllib2
-import shutil
-import json
+import time
+import datetime
 import codecs
 import re
 import HTMLParser
@@ -56,7 +55,7 @@ def clean_audio_tag(tag):
     tag = re.sub(r'http://.[^\s]+', '', tag)  # remove any urls
     tag = tag.replace(' :)','')  # remove smiles
 
-    tag = re.sub('’', '', tag).strip()  # quotation mark -> apostrophe
+    tag = re.sub('’', '\'', tag).strip()  # quotation mark -> apostrophe
     ctag = re.compile(u'[^\w\s_\.,&#!?\-\'"`\/\|\[\]\(\)]', re.U)
     tag = ctag.sub('', tag).strip()  # kill most unusual symbols
     tag = re.sub(r'\s+', ' ', tag)  # remove long spaces
@@ -78,6 +77,8 @@ def set_id3(filename, lyr_text, **track):
 
 
 def save_tracks(filename, tracks):
+    """saves all tracks' info into a playlist file
+    """
     if not tracks:
         return
 
@@ -91,15 +92,23 @@ def save_tracks(filename, tracks):
 
 
 def open_tracks(filepath):
+    """Loads tracks from a file into a list
+    """
     with codecs.open(filepath, 'r', 'utf8') as fp:
         firstline = fp.next()
         fields = firstline.rstrip('\n').split('\t')
         for line in fp:
             track = dict(zip(fields, line.rstrip('\n').split('\t')))
+            track['duration'] = int(track['duration'])
+            if track['genre'] != '':
+                track['genre'] = int(track['genre'])
+            track['aid'] = int(track['aid'])
+            track['owner_id'] = int(track['owner_id'])
             yield track
 
 
-def download_tracks(tracks, token, uid, storage_path='files'):
+def download_tracks(tracks, token, uid, login, storage_path='files'):
+    # todo support of case, when tracks got renamed and lyrics got edited
     if tracks and not os.path.exists(storage_path):
         os.makedirs(storage_path)
 
@@ -113,7 +122,6 @@ def download_tracks(tracks, token, uid, storage_path='files'):
         filepath = os.path.join(storage_path, "%s_%s" % (track.get('aid'), filename))
 
         if os.path.isfile(filepath):
-            # todo support aborted downloads
             print 'Skipped "%(artist)s - %(title)s"' % (track)
             continue
 
@@ -128,7 +136,7 @@ def download_tracks(tracks, token, uid, storage_path='files'):
             if total:
                 bar = ProgressBar(maxval=int(total)).start()
 
-            with open(filepath, 'wb') as fp:
+            with open('temp.mp3', 'wb') as fp:
                 chunk_size = 16 * 1024
                 loaded = 0
 
@@ -138,20 +146,21 @@ def download_tracks(tracks, token, uid, storage_path='files'):
                     if total:
                         loaded += len(chunk)
                         bar.update(loaded)
-
+            os.rename('temp.mp3', filepath)
             if total:
                 bar.finish()
 
-            lyr_text = ''
-            if track.get('lyrics_id'):
-                print "Lyrics exist!"
-                track['lyrics_id'] = clean_audio_tag(track.get('lyrics_id'))
-                res = vk_api.call_method('audio.getLyrics', {'lyrics_id': track['lyrics_id']}, token)
-                lyr_text = res.get('response')['text']
-                with codecs.open(filepath + '.txt', 'w', encoding='utf-8') as lyr_file:
-                    lyr_file.write(lyr_text)
-                    lyr_file.close()
-            set_id3(filepath, lyr_text, **track)
+            if login:
+                lyr_text = ''
+                if track.get('lyrics_id'):
+                    print "Lyrics exist!"
+                    track['lyrics_id'] = clean_audio_tag(track.get('lyrics_id'))
+                    res = vk_api.call_method('audio.getLyrics', {'lyrics_id': track['lyrics_id']}, token)
+                    lyr_text = res.get('response')['text']
+                    with codecs.open(filepath + '.txt', 'w', encoding='utf-8') as lyr_file:
+                        lyr_file.write(lyr_text)
+                        lyr_file.close()
+                set_id3(filepath, lyr_text, **track)
             track_cnt += 1
 
         except urllib2.HTTPError, err:
@@ -161,10 +170,55 @@ def download_tracks(tracks, token, uid, storage_path='files'):
             print "IOError:", err
 
 
-def main():
+def compare_playlists(tracks, previous_tracks):
+    """Provides info about how many tracks were deleted, added and renamed compared to the previous playlist
+    """
+    import copy
+    print 'Comparing saved playlist with the updated version'
+    start_time = time.time()
+    deleted = copy.deepcopy(previous_tracks)
+    added = copy.deepcopy(tracks)
+    changed = 0
 
-    playlist = 'playlist.txt'
-    tracks = []
+    for trck_old in previous_tracks:
+        for trck_new in tracks:
+            if int(trck_new['aid']) == int(trck_old['aid']):
+                deleted.remove(trck_old)
+                added.remove(trck_new)
+                if trck_new['artist'] != trck_old['artist'] or trck_new['title'] != trck_old['title']:
+                    changed += 1
+    print changed, ' tracks renamed'
+    print len(added), ' new tracks added'
+    for trck in added:
+        print repr('    ' + trck['artist'] + u' — ' + trck['title']).decode("unicode-escape")
+    print len(deleted), ' old tracks deleted'
+    for trck in deleted:
+        print repr('    ' + trck['artist'] + u' — ' + trck['title']).decode("unicode-escape")
+    print 'Elapsed time on comparison', time.time()-start_time
+    response = raw_input("Would you like to physically delete those tracks that were downloaded to your device but removed on VK? y/n:")
+    if response.lower() == 'y':
+        for trck in deleted:
+            regex_match = re.search('/([a-z0-9]+\.mp3)\?extra=', trck['url'])
+            if regex_match is None:
+                print 'Can\'t find track filename in the URL'
+            else:
+                track_path = os.path.join('files', "%s_%s" % (trck.get('aid'), regex_match.group(1)))
+                if os.path.isfile(track_path):
+                    os.remove(track_path)
+                    print 'Deleted!'
+                else:
+                    print 'No such file found!', trck['artist'], u'—', trck['title']
+    print 'All new files will be downloaded automatically'
+
+
+def main():
+    new_playlist_file = 'playlist.txt'  # a temporary playlist file, that gets deleted after all tracks are downloaded
+    playlists_folder = 'playlists'  # a folder for all previous playlist files
+    if not os.path.exists(playlists_folder):
+        os.makedirs(playlists_folder)
+    today_date = str(datetime.date.today())
+    prev_playlist_file = sorted(os.listdir(playlists_folder))[-1]
+    print prev_playlist_file
 
     # authorization (needed for lyrics and albums)
     user = {
@@ -174,23 +228,37 @@ def main():
     }
     client_id = config.CLIENT_ID
     token, uid = get_token(client_id, **user)
-
-    if not os.path.isfile(playlist):
-        tracks = get_audio(token, uid)
-        save_tracks(playlist, tracks)
-        res_albums = vk_api.call_method('audio.getAlbums', {'owner_id': uid}, token).get('response')[1:]
-        for album in res_albums:
-            print 'Saving album ' + album['title']+ str(album['album_id'])
-            res_alb_tracks = get_audio_alb(token, uid, album['album_id'])
-            save_tracks('alb_' + str(album['album_id']) + '_' + album['title'], res_alb_tracks)
+    print u'Connected to VK servers!'
+    response = raw_input("Do you wish to download albums and lyrics? y/n: ")
+    if response.lower() == 'y':
+        login = True
     else:
-        tracks = list(open_tracks(playlist))
+        login = False
 
-    download_tracks(tracks, token, uid, 'files')
+    if not os.path.isfile(new_playlist_file):
+        # if no temporary @playlist file — we download the latest playlist version and compare with the previous one
+        tracks = get_audio(token, uid)
+        prev_tracks = list(open_tracks(os.path.join(playlists_folder, prev_playlist_file)))
+        save_tracks(new_playlist_file, tracks)
+        save_tracks(os.path.join(playlists_folder, today_date + '_main_' + new_playlist_file), tracks)  # a duplicate of the new playlist is saved for backup
+        compare_playlists(tracks, prev_tracks)
+        if login:
+            # downloading separate album playlists
+            print 'downloading album playlists'
+            res_albums = vk_api.call_method('audio.getAlbums', {'owner_id': uid}, token).get('response')[1:]
+            for album in res_albums:
+                title = unicode(album['title']).strip()
+                print u'Saving album ' + title.strip() + u' with id = ' + str(album['album_id'])
+                res_alb_tracks = get_audio_alb(token, uid, album['album_id'])
+                time.sleep(.500)  # accessing VK requires some timeout, otherwise request get ignored
+                save_tracks(os.path.join(playlists_folder, u'albums', today_date + '_' + str(album['album_id']) + u'.txt'), res_alb_tracks)
+    else:
+        tracks = list(open_tracks(new_playlist_file))
 
-    print 'done.'
+    download_tracks(tracks, token, uid, login, 'files')
+    os.remove(new_playlist_file)
+    print 'All tracks downloaded.'
 
 
 if __name__ == '__main__':
     main()
-
